6b278773ef2cf903201ea980a938ef17d0f0bbb5
Daniel Huisman <daniel@huisman.me>
2022-04-05 00:04:59 +0200

    Add Bootstrap color variables

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useTranslation} from 'react-i18next';
 
 export const Header: React.FC = () => {
+    const {t} = useTranslation();
+
     return (
         <header>
             <h1>{t('general:meta.title', 'transgenderwachttijd.nl')}</h1>
