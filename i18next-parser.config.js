4787ed74260fe98f9fd2f3f45bca58e2ff66f218
Daniel Huisman <daniel@huisman.me>
2022-08-29 20:42:01 +0200

    Customize code style and add i18n support

---
+++ i18next-parser.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    indentation: 4,
+    input: [
+        '**/*.{ts,tsx}'
+    ],
+    locales: ['en', 'nl'],
+    output: 'public/locales/$LOCALE/$NAMESPACE.json',
+    sort: true
+};
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
 
-module.exports = nextConfig
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'nl']
+    }
+};
+
+module.exports = nextConfig;
