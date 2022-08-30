e776350b9c0527dffc814ad9ed1e122d202ff370
Daniel Huisman <daniel@huisman.me>
2022-08-30 17:56:40 +0200

    Add basic select dropdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type {GetStaticProps, NextPage} from 'next';
-// import {useTranslation} from 'next-i18next';
+import {useTranslation} from 'next-i18next';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {Container} from 'reactstrap';
+
+import {SelectDropdown} from '../components/SelectDropdown';
 
 export const getStaticProps: GetStaticProps = async ({locale}) => ({
     props: {
@@ -10,10 +12,27 @@ export const getStaticProps: GetStaticProps = async ({locale}) => ({
 });
 
 const Home: NextPage = () => {
-    // const {t} = useTranslation();
+    const {t} = useTranslation();
 
     return (
         <Container>
+            <SelectDropdown
+                label={t('providers.name', 'Providers')}
+                options={[{
+                    value: '1',
+                    label: 'Amsterdam UMC'
+                }, {
+                    value: '2',
+                    label: 'Radboudumc'
+                }, {
+                    value: '3',
+                    label: 'UMC Groningen'
+                }]}
+                idPrefix="providers"
+                buttonLabel="Update"
+                toggleProps={{color: 'primary', outline: true, style: {width: '10rem'}}}
+            />
+
             TODO
         </Container>
     );
