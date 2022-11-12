32f356fb20a75490ae0c2bb11d9da65743a798a9
Daniel Huisman <daniel@huisman.me>
2022-11-12 22:26:57 +0100

    Add graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
+import {ApolloProvider} from '@apollo/client';
 import type {AppProps} from 'next/app';
 import {appWithTranslation} from 'next-i18next';
 
 import '../styles/globals.scss';
 import {Layout} from '../components/Layout';
+import {client} from '../graphql/apollo-client';
 
 const App = ({Component, pageProps}: AppProps) => (
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    </ApolloProvider>
 );
 
 export default appWithTranslation(App);
